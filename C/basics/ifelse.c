5052c48489f5be40472144eb4b453bbed95b4287
Tevin Mantock <tmantock@gmail.com>
2016-12-17 20:20:59 -0800

    if block scope for single command

--- C/basics/ifelse.c
+++ C/basics/ifelse.c
@@ -3,6 +3,9 @@
 int main(){
     int raining = 0;
 
+    if(raining == 0)
+        printf("Dude it's not raining in Seattle? \n");
+
     if(raining == 1){
         printf("It is raining get warm. \n");
     } else {
