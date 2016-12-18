1a82fea78e4cd560d73f77f970ef6ea76770e846
Tevin Mantock <tmantock@gmail.com>
2016-12-17 19:41:05 -0800

    c basic types

---
+++ C/basics/types.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+
+    int x = 10;
+    short y = 90;
+    long z = 5000671;
+
+    printf("%d, %d, %ld \n", x, y, z);
+
+    short unsigned int students = 65535;
+    long unsigned int teachers = 4294967295;
+
+    printf("%ud, %lu \n", students, teachers);
+
+    float fl = 2.718;
+    double db = 3.145;
+    long double ldb = 4.555483847645;
+    printf("%lf, %lf, %Lf \n", fl, db, ldb);
+
+    char a = 'a';
+    char b = 'b';
+    printf("%c, %c \n", a, b);
+
+    return 0;
+}
