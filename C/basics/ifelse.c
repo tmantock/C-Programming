b7605c5d43dc138d8220c87c15c2ea61ea1c353f
Tevin Mantock <tmantock@gmail.com>
2016-12-17 20:36:36 -0800

    not equal to

--- C/basics/ifelse.c
+++ C/basics/ifelse.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main(){
-    int raining = 0;
+    int raining = 1;
+    int tired = 1;
 
-    if(raining == 0)
+    if(raining != 1)
         printf("Dude it's not raining in Seattle? \n");
 
-    if(raining == 1){
+    if(raining == 1 && tired == 1){
         printf("It is raining get warm. \n");
     } else {
         printf("Get to work. \n");
