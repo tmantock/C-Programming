961762a8f26f1eddc8fdb9b1eb0bad7f92f82242
Tevin Mantock <tmantock@gmail.com>
2016-12-17 23:47:30 -0800

    update

--- C/basics/ifelse.c
+++ C/basics/ifelse.c
@@ -3,6 +3,7 @@
 int main(){
     int raining = 1;
     int tired = 1;
+    int age = 18;
 
     if(raining != 1)
         printf("Dude it's not raining in Seattle? \n");
@@ -11,5 +12,11 @@ int main(){
         printf("It is raining get warm. \n");
     } else {
         printf("Get to work. \n");
+    }
+
+    if( age >= 18){
+        printf("You are an adult \n");
+    } else {
+        printf("You're young one \n");
     }
 }
