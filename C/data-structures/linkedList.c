a9dfc09bb0fc5ac96677da85705f48fc77ab2283
Tevin Mantock <tmantock@gmail.com>
2017-04-09 14:45:17 -0700

    append and get

--- C/data-structures/linkedList.c
+++ C/data-structures/linkedList.c
@@ -10,6 +10,7 @@ int get_length(struct node* head);
 node* generate_node(struct node* head);
 int generate_nodes(struct node* last, int length);
 int free_nodes(struct node* n);
+node* get_nth(struct node* head, int n);
 
 int main(void){
     node head;
@@ -20,13 +21,17 @@ int main(void){
 
     int length = get_length(head.next);
 
+    node *n = get_nth(&head, 3);
+
+    printf("Node 3: %p\n", n);
+
     printf("%d\n", length);
 
     free_nodes(&head);
 
-    int l = get_length(&head);
+    // int l = get_length(&head);
 
-    printf("%d\n", l);
+    // printf("%d\n", l);
 }
 
 node* generate_node(struct node* previous) {
@@ -66,12 +71,30 @@ int generate_nodes(struct node* last, int length) {
 int get_length(struct node* head) {
     int length = 0;
     while(head != NULL) {
-        printf("Data: %d\n", head->data);
+        printf("Data %d: %p\n", length, head);
         length++;
         head = head->next;
     }
 
     return length;
+}
+
+node* get_nth(struct node* head, int n) {
+    if(n < 0) {
+        return NULL;
+    }
+
+    if(get_length(head) < n) {
+        return NULL;
+    }
+    
+    int c = 0;
+    while(head->next != NULL && c < n) {
+        c++;
+        head = head->next;
+    }
+
+    return head;
 }
 
 int free_nodes(struct node* n) {
@@ -97,4 +120,24 @@ int free_nodes(struct node* n) {
 
     // All good
     return 0;
+}
+
+void append_node(struct node** headref, int data) {
+    assert(headref != NULL);
+
+    if(*headref == NULL) {
+        *headref = (struct node*)malloc(sizeof(struct node));
+        (*headref)->data = data;
+        (*headref)->next = NULL;
+        return;
+    }
+
+    struct node* head = *headref;
+    while(head->next != NULL) {
+        head = head->next;
+    }
+
+    head->next = (struct node*)malloc(sizeof(struct node));
+    head->next->data = data;
+    head->next->next = NULL;
 }
