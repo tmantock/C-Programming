5febeba0144c2247ab4e31a26486af58ec94defd
Tevin Mantock <tmantock@gmail.com>
2017-04-30 13:38:07 -0700

    classes

---
+++ C++/Cstart/class.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+class ComplexNumber {
+    private:
+        float real;
+        float complex;
+    public:
+        ComplexNumber() : real(0.0), complex(0.0) {
+            cout << "No arg-constructot called" << endl;
+        }
+
+        void setNumbers(float r, float c) {
+            real = r;
+            complex = c;
+        }
+
+        float getReal() {
+            return real;
+        }
+
+        float getComplex() {
+            return complex;
+        }
+
+        void print() {
+            cout << "Real " << real << " Complex " << complex << endl;
+        }
+
+        ~ComplexNumber() {
+            cout << "Freeing ComplexNumber" << endl;
+        }
+};
+
+class Student {
+    private:
+        char * name;
+    public:
+        Student(const char * n) {
+            name = new char[50];
+            strcpy(name, n);
+            cout << "Initialized string to " << name << endl;
+        }
+
+        ~Student() {
+            cout << "Freeing student " << name << endl;
+            delete[] name;
+        }
+};
+
+int main() {
+    ComplexNumber c;
+    c.print();
+    c.setNumbers(3.54, 69.69);
+    c.print();
+    
+    const char name[10] = "Tevin";
+    Student s(name);
+    cout << "Exiting" << endl;
+}
